afd188abe224848ae301c3fb3c7dc8c9c1fa07a4
Surumi00 <surumika44@gmail.com>
2026-07-13 10:13:59 +0530

    add-donour

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Homepage from './assets/components/Homepage'
+import AddDonour from './assets/components/AddDonour'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Homepage/>
+      <AddDonour/>
     </>
   )
 }
